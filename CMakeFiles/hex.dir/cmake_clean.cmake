0ac89b1f8700712c51482c3aebd84fa2df75308d
Killian4096 <killianrush203@gmail.com>
2022-10-31 18:16:40 -0400

    Initial commit, got interrupt + tx working

---
+++ CMakeFiles/hex.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hex"
+  "weather_station.eeprom"
+  "weather_station.hex"
+  "weather_station.lst"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/hex.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ CMakeFiles/strip.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/strip"
+  "weather_station.eeprom"
+  "weather_station.hex"
+  "weather_station.lst"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/strip.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
